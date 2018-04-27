b1d9ef28a5bd2e970b68d02aeb7dfce936d3e9d7
FastFeline <lucy7anderson@gmail.com>
2018-04-27 08:04:35 -0700

    Gives multiples of a user chosen number up to 13

---
+++ multiplier.py
@@ -0,0 +1,48 @@
+#Multiplication to 13 in Python
+print("Welcome to Multiplier in Python")
+base = input("What number shall I list multiples of?")
+base = int(base)
+print(base)
+
+print("Great!")
+print(str(base) + "* 0 = \n 0")
+
+print(str(base) + "* 1 =")
+onemultiple = base*1
+print(onemultiple)
+print(str(base) + "* 2 =")
+twomultiple = base*2
+print(twomultiple)
+print(str(base) + "* 3 =")
+threemultiple = base*3
+print(threemultiple)
+print(str(base) + "* 4 =")
+fourmultiple = base*4
+print(fourmultiple)
+print(str(base) + "* 5 =")
+fivemultiple = base*5
+print(fivemultiple)
+print(str(base) + "* 6 =")
+sixmultiple = base*6
+print(sixmultiple)
+print(str(base) + "* 7 =")
+sevenmultiple = base*7
+print(sevenmultiple)
+print(str(base) + "* 8 =")
+eightmultiple = base*8
+print(eightmultiple)
+print(str(base) + "* 9 =")
+ninemultiple = base*9
+print(ninemultiple)
+print(str(base) + "* 10 =")
+tenmultiple = base*10
+print(tenmultiple)
+print(str(base) + "* 11 =")
+elevenmultiple = base*11
+print(elevenmultiple)
+print(str(base) + "* 12 =")
+twelvemultiple = base*12
+print(twelvemultiple)
+print(str(base) + "* 13 =")
+thirteenmultiple = base*13
+print(thirteenmultiple)
