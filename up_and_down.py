f8e57ad1643d1dd81e0cb8dcb1090de64497e429
FastFeline <lucy7anderson@gmail.com>
2018-06-07 06:47:05 -0700

    Counts from 1 to 100, then from 100 to 1.

---
+++ up_and_down.py
@@ -0,0 +1,11 @@
+import time
+for i in range(0,101):
+    print(i)
+    time.sleep(0.0625)
+print("\n Counting back! \n")
+time.sleep(5)
+print("Here we go! \n")
+time.sleep(.333)
+for y in range(100,0,-1):
+    print(y)
+    time.sleep(0.0625)
