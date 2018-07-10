42a923cf5c148ea2c41df7fbc3c27c1fcbde31a3
FastFeline <lucy7anderson@gmail.com>
2018-07-09 20:07:52 -0700

    toot

---
+++ fart.py
@@ -0,0 +1,83 @@
+print("Someone Farted!")
+print("But... who?")
+import random      #imports a thing: random number generator
+for x in range(10): #range of 1 to 10
+  randomvar = random.randint(1,10)  #gets random number from 1 to 10
+if(randomvar == 1):
+    print("A Skunk")
+elif(randomvar == 2):
+    print("Stevie the Inflatable Alligator")
+elif(randomvar == 3):
+    print("Cousin Larry")
+elif(randomvar == 4):
+    print("Santa Claus.")
+elif(randomvar == 5):
+    print("A Dog")
+elif(randomvar == 6):
+    print("My Cat, Blackie")
+elif(randomvar == 7):
+    print("Your Annoying Little Sibling")
+elif(randomvar == 8):
+    print("The Stranger sitting next to you on the bus!")
+elif(randomvar == 9):
+    print("A FREAKING UNICORN!!")
+else:
+    fillblank = input("Fill in the Blank")
+    print ("This is who farted: ")
+    print(fillblank)
+
+#The Fart
+print("The Fart smells like ")
+import random
+for x in range(10):
+  randomvar = random.randint(1,10)
+if(randomvar == 1):
+    print("A Skunk")
+elif(randomvar == 2):
+    print("Dad's Pits")
+elif(randomvar == 3):
+    print("The Pits of hell")
+elif(randomvar == 4):
+    print("A Fart. Du-uh.")
+elif(randomvar == 5):
+    print("Cabbage")
+elif(randomvar == 6):
+    print("A Wet Dog")
+elif(randomvar == 7):
+    print("Rotten Eggs")
+elif(randomvar == 8):
+    print("Sulfur")
+elif(randomvar == 9):
+    print("Poo")
+else:
+    fillblank2 = input("Fill in the Blank")
+    print ("The Fart Smells Like: ")
+    print(fillblank2)
+
+#Reaction
+print("What do you do?")
+import random
+for x in range(10):
+  randomvar = random.randint(1,10)
+if(randomvar == 1):
+    print("Run away screaming")
+elif(randomvar == 2):
+    print("Ignore it")
+elif(randomvar == 3):
+    print("Spray perfume everywhere")
+elif(randomvar == 4):
+    print("Stare at them accusingly")
+elif(randomvar == 5):
+    print("yell WHO FARTED!?")
+elif(randomvar == 6):
+    print("try and hold your breath")
+elif(randomvar == 7):
+    print("gag")
+elif(randomvar == 8):
+    print("throw pop tarts at whoever did it")
+elif(randomvar == 9):
+    print("put on a gas mask")
+else:
+    fillblank3 = input("Fill in the Blank")
+    print ("This is what I do: ")
+    print(fillblank3)
