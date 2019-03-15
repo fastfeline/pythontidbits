788ba0d1315388a88c9dc71b6598dad5ab0168bc
FastFeline <lucy7anderson@gmail.com>
2019-03-14 17:15:22 -0700

    I added more features

--- fart.py
+++ fart.py
@@ -1,26 +1,28 @@
 print("Someone Farted!")
 print("But... who?")
 import random      #imports a thing: random number generator
-for x in range(10): #range of 1 to 10
-  randomvar = random.randint(1,10)  #gets random number from 1 to 10
-if(randomvar == 1):
+for x in range(12): #range of 1 to 12
+  culprit = random.randint(1,12)  #gets random number from 1 to 12
+if(culprit == 1):
     print("A Skunk")
-elif(randomvar == 2):
+elif(culprit == 2):
     print("Stevie the Inflatable Alligator")
-elif(randomvar == 3):
+elif(culprit == 3):
     print("Cousin Larry")
-elif(randomvar == 4):
+elif(culprit == 4):
     print("Santa Claus.")
-elif(randomvar == 5):
+elif(culprit == 5):
     print("A Dog")
-elif(randomvar == 6):
+elif(culprit == 6):
     print("My Cat, Blackie")
-elif(randomvar == 7):
+elif(culprit == 7):
     print("Your Annoying Little Sibling")
-elif(randomvar == 8):
+elif(culprit == 8):
     print("The Stranger sitting next to you on the bus!")
-elif(randomvar == 9):
+elif(culprit == 9):
     print("A FREAKING UNICORN!!")
+elif(culprit == 10):
+    print("Fawkes the phoenix.")
 else:
     fillblank = input("Fill in the Blank")
     print ("This is who farted: ")
@@ -29,8 +31,8 @@ else:
 #The Fart
 print("The Fart smells like ")
 import random
-for x in range(10):
-  randomvar = random.randint(1,10)
+for x in range(12):
+  randomvar = random.randint(1,12)
 if(randomvar == 1):
     print("A Skunk")
 elif(randomvar == 2):
@@ -49,6 +51,8 @@ elif(randomvar == 8):
     print("Sulfur")
 elif(randomvar == 9):
     print("Poo")
+elif(randomvar == 10):
+    print("Cat Poo")
 else:
     fillblank2 = input("Fill in the Blank")
     print ("The Fart Smells Like: ")
@@ -57,27 +61,65 @@ else:
 #Reaction
 print("What do you do?")
 import random
-for x in range(10):
-  randomvar = random.randint(1,10)
-if(randomvar == 1):
+for x in range(12):
+  Reaction = random.randint(1,12)
+if(Reaction == 1):
     print("Run away screaming")
-elif(randomvar == 2):
+elif(Reaction== 2):
     print("Ignore it")
-elif(randomvar == 3):
+elif(Reaction == 3):
     print("Spray perfume everywhere")
-elif(randomvar == 4):
+elif(Reaction == 4):
     print("Stare at them accusingly")
-elif(randomvar == 5):
-    print("yell WHO FARTED!?")
-elif(randomvar == 6):
-    print("try and hold your breath")
-elif(randomvar == 7):
-    print("gag")
-elif(randomvar == 8):
-    print("throw pop tarts at whoever did it")
-elif(randomvar == 9):
-    print("put on a gas mask")
+elif(Reaction == 5):
+    print("Yell 'WHO FARTED!?' ")
+elif(Reaction == 6):
+    print("Try and hold your breath")
+elif(Reaction == 7):
+    print("Gag/barf")
+elif(Reaction == 8):
+    print("Throw pop tarts at whoever did it")
+elif(Reaction == 9):
+    print("Put on a gas mask")
+elif(Reaction == 10):
+    print("Throw your invisibility cloak over yourself and quietly slip out the door.")
+
 else:
     fillblank3 = input("Fill in the Blank")
     print ("This is what I do: ")
+    print(fillblank3)
+
+#Interaction
+print("What does the farter do?")
+import random
+for x in range(12):
+  randomvar = random.randint(1,12)
+if(randomvar == 1):
+    print("Begins to cry and declares, 'I'm so sorry!'")
+elif(randomvar == 2):
+    print("Shrugs and shovels more beans into their mouth")
+elif(randomvar == 3):
+    print("Blames it on the person next to them")
+elif(randomvar == 4):
+    print("Blames it on you, saying, 'Ya smelt it, ya dealt it!'")
+elif(randomvar == 5):
+    print("Owns up to it")
+elif(randomvar == 6):
+    print("Runs away")
+elif(randomvar == 7):
+    print("Farts again")
+elif(Reaction == 8 ):
+    print("Catches the pop tart and eats it")
+elif(randomvar == 8):
+    print("Blush")
+elif(culprit == 8 or randomvar == 9):
+    print("Gets kicked off the bus")
+elif(randomvar == 10 and Reaction != 10):
+    print("Throws their invisibility cloak over themself and quietly slip out the door.")
+elif(randomvar == 10 and Reaction == 10):
+    print("Giggles")
+
+else:
+    fillblank3 = input("Fill in the Blank")
+    print ("The source of the fart...")
     print(fillblank3)
